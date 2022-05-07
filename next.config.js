7e03325bc0c3b2b9b16df06ccb172cd98cf36aa0
Manabu OHTAKE <manabu2783@hotmail.com>
2022-05-07 11:39:56 +0900

    :sparkles: src ディレクトリに変更

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  webpack: (configuration) => {
+    configuration.resolve.alias['@'] = path.join(__dirname, './src')
+    return configuration
+  },
 }
 
 module.exports = nextConfig
