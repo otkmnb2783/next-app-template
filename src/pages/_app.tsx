33aebcb65aa8c029a661547e541d7a0cedc0ec7b
Manabu OHTAKE <manabu2783@hotmail.com>
2022-05-08 14:48:51 +0900

    fix: :heavy_minus_sign: nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,7 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
-import nprogress from 'nprogress'
-import { useEffect } from 'react'
 
-nprogress.configure({ showSpinner: false, speed: 400, minimum: 0.25 })
-
-function MyApp({ Component, pageProps, router }: AppProps) {
-  if (window) {
-    nprogress.start()
-  }
-  useEffect(() => {
-    nprogress.done()
-  }, [router.pathname])
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
